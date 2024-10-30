3f7c31a98597b46a2b3d938c617c22184218a47c
ShakOffWhite <98908218+ShakOffWhite@users.noreply.github.com>
2024-10-30 09:52:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import subprocess
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
 
 @app.route('/run', methods=['POST'])
 def run_exe():
     try:
         # Укажите путь к вашему .exe файлу
-        result = subprocess.run(['path/to/your/menuu.exe'], capture_output=True, text=True)
+        result = subprocess.run(['C:/Users/v.morozov/Desktop/My test/teams-app/menuu.exe'], capture_output=True, text=True)
         return jsonify({'output': result.stdout, 'error': result.stderr}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
