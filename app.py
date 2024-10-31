2ebcbc2a39596d1dae1ca0373af38a41ee485008
ShakOffWhite <98908218+ShakOffWhite@users.noreply.github.com>
2024-10-31 09:59:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,14 @@ def clear_cache():
     )
     return jsonify({'instructions': instructions})
 
+@app.route('/feature-2', methods=['POST'])
+def feature-2():
+    instructions = (
+        "Нажміть на ссилку:\n"
+        "1. https://bimleaders-my.sharepoint.com/personal/hryhoriv_bim-ae_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fhryhoriv%5Fbim%2Dae%5Fcom%2FDocuments%2F1%20Install%2F00%20Instructions%2FUA%20%D0%86%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%86%D1%96%D1%8F%20%D0%B4%D0%BB%D1%8F%20%D0%BD%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE%20%D0%BF%D1%80%D0%B0%D1%86%D1%96%D0%B2%D0%BD%D0%B8%D0%BA%D0%B0%20v%2E2%2E7%2Epdf&parent=%2Fpersonal%2Fhryhoriv%5Fbim%2Dae%5Fcom%2FDocuments%2F1%20Install%2F00%20Instructions&ga=1'."
+    )
+    return jsonify({'instructions': instructions})
+    
 @app.route('/restart-service', methods=['POST'])
 def restart_service():
     instructions = (
